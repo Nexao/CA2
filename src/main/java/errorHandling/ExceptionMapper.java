538bd43eac844b940cacbe51b888299dee649154
Diana <Diana@192.168.8.101>
2016-10-04 09:17:53 +0200

    Added error mapper and message class :)

--- src/main/java/errorHandling/ExceptionMapper.java
+++ src/main/java/errorHandling/ExceptionMapper.java
@@ -5,10 +5,27 @@
  */
 package errorHandling;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
 /**
  *
  * @author Diana
  */
-public class ExceptionMapper {
+@Provider
+public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception>{
+
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    @Context 
+    ServletContext context;
+    
+    @Override
+    public Response toResponse(Exception e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
